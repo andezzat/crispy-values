36691832c3bc772207f9d25d41a3500535483f5a
Andrew Salib <andezzat@gmail.com>
2017-08-25 15:53:05 +1000

    Extra stuff

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: __dirname + '/app/index.js',
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
