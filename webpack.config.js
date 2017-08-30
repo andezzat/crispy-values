8d64c2628d3f42eb60a1cfc8ca1b9b71f29d1858
Andrew Salib <andezzat@gmail.com>
2017-08-30 23:49:59 +1000

    Configure Webpack to handle SASS files, fonts and images

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
-// const HTMLWebpackPlugin = require('html-webpack-plugin');
-//
-// const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-//   template: __dirname + '/app/index.html',
-//   filename: 'index.html',
-//   inject: 'body'
-// });
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: __dirname + '/src/client/app.jsx',
+  entry: [
+    __dirname + '/src/client/app.jsx',
+    __dirname + '/scss/theme.scss',
+  ],
 
   output: {
     filename: 'app.js',
-    path: __dirname + '/public/javascripts/'
+    path: __dirname + '/public/javascripts'
   },
 
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' }
+      { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']) },
+      { test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000&name=[name].[ext]&outputPath=../fonts/' },
     ]
   },
-  // plugins: [HTMLWebpackPluginConfig]
-};
+  plugins: [
+    new ExtractTextPlugin('../stylesheets/styles.css', {
+      allChunks: true,
+    }),
+  ],
+}
