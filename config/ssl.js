fc7c04a57b81262c280211c318faed4d44cfb7d8
Andrew Salib <andezzat@gmail.com>
2017-11-06 13:30:47 +1100

    Delete SSL
    Won't need it since we need to configure SSL & HTTPS on nginx level

--- config/ssl.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  production: {
-    key: '/etc/letsencrypt/live/valuesfootprint.com/fullchain.pem',
-    cert: '/etc/letsencrypt/live/valuesfootprint.com/privkey.pem'
-  }
-};
--- config/ssl_example.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  production: {
-    key: 'directory/example_key.pem',
-    cert: 'directory/example_cert.pem'
-  }
-};
