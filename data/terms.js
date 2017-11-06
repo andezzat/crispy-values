f99e636c0c0cd751d89e724b2b3ea2ae8476da6a
erin-castellas <33405080+erin-castellas@users.noreply.github.com>
2017-11-06 11:59:11 +1100

    Insert start of terms text

--- data/terms.js
+++ data/terms.js
@@ -4,9 +4,14 @@ module.exports = {
   sections: [
     {
       text: [
-        `We are committed to safeguarding the privacy of our website visitors and this policy sets out how we will treat your personal information. Where we process personal data we shall do so in accordance with the provisions of the Privacy Act 1988.`,
-        `Our website uses cookies. By using our website and agreeing to this policy, you consent to our use of cookies in accordance with the terms of this policy.`
-      ]
+        `Services provided on this website, titled the “Social Enterprise Reporting Tool”, are provided by the Centre for Social Impact, Swinburne University of Technology (“Swinburne”). Please read the Terms of Use carefully, as registering for an account on this site indicates that you agree to abide by these conditions. If you do not wish to be bound by the Terms of Use, 
+         you are not permitted to register for, or use, the Social Enterprise Reporting Tool service.`      ]
+    },
+    {
+      heading: 'Acceptance of Terms of Use',
+      text: [
+        `Each time you use or cause access to the Social Enterprise Reporting Tool website, you agree to be bound by the site’s Terms of Use. If you use or access a particular service on or through this website, you will also be subject to any rules or guidelines applicable to that service.  
+         Please note if the service or website is a third party site, Swinburne has no control over that site.`      ]
     }
   ]
 };
