04d370c909e64e2b557dd6179a44ab66ab0de2cf
Andrew Salib <andezzat@gmail.com>
2017-11-10 19:54:13 +1100

    Update terms.js
    - Update terms.js with latest information from terms of use document sent by erin via email

--- data/terms.js
+++ data/terms.js
@@ -3,15 +3,103 @@ module.exports = {
   subtitle: 'Our terms of use are outlined below, please read them carefully.',
   sections: [
     {
+      heading: 'We Value Your Privacy',
       text: [
-        `Services provided on this website, titled the “Social Enterprise Reporting Tool”, are provided by the Centre for Social Impact, Swinburne University of Technology (“Swinburne”). Please read the Terms of Use carefully, as registering for an account on this site indicates that you agree to abide by these conditions. If you do not wish to be bound by the Terms of Use, 
-         you are not permitted to register for, or use, the Social Enterprise Reporting Tool service.`      ]
+        `Swinburne University of Technology (Swinburne) through the VALUES FOOTPRINT study team, acknowledges and respects the privacy of individuals.
+        Given the nature of the online service provided on the VALUES FOOTPRINT website, you may have provided, or be asked to provide,
+        ‘identifying information’ (i.e. information that is unique to you or your organisation, and identifies you or your organisation),
+        as well as information that may be considered confidential or sensitive (i.e. information that is not necessarily unique to you or your organisation,
+        but which you could reasonably expect to be managed respectfully). This policy outlines what information is recorded on the VALUES FOOTPRINT website;
+        the purpose of collecting this information; how the information is used and protected,
+        and your rights and responsibilities with respect to the security of the personal information you provide, as a user of the VALUES FOOTPRINT.`
+      ]
     },
     {
-      heading: 'Acceptance of Terms of Use',
+      heading: 'What Information Do We Collect?',
       text: [
-        `Each time you use or cause access to the Social Enterprise Reporting Tool website, you agree to be bound by the site’s Terms of Use. If you use or access a particular service on or through this website, you will also be subject to any rules or guidelines applicable to that service.  
-         Please note if the service or website is a third party site, Swinburne has no control over that site.`      ]
+        `Information is provided to VALUES FOOTPRINT from users of the site, through the completion of an online questionnaire.`,
+        `The online questionnaire requires all users to provide demographic information, such as age and gender.
+        Personally identifying information, such as email address and promotion code (relating a particular group study) are optional.
+        All subsequent information regarding site use, such as log in times and dates and responses to online surveys is matched to the demographic information input by the user.`,
+        `The following information is electronically recorded against the demographic details of VALUES FOOTPRINT users: your responses to the VALUES FOOTPRINT questionnaire.`,
+        `Users are also asked to optionally provide a valid email address.
+        The purpose of collecting this information is so that you can be contacted in relation to a group study if you have chosen to participate in one.`,
+        `In addition to the information that users provide through their registration and use of the VALUES FOOTPRINT website,
+        users may contact the researchers via the email address: valuesfootprint@gmail.com. By doing so,
+        the research team member will receive your email address and any personal information you provide in your email correspondence.`
+      ]
+    },
+    {
+      heading: 'Why Do We Collect Your Information?',
+      text: [
+        `As a provider of an online service, the VALUES FOOTPRINT website administrators do not request users to provide identifying, sensitive or confidential information.`,
+        `Although the user is asked to optionally provide an email address,
+        this information is not shared or used for any purposes outside of those described in the VALUES FOOTPRINT website Terms of use.`,
+        `The data collected by the VALUES FOOTPRINT website will be de-identified, collated,
+        and reported in group format in any publications or presentations arising from this research.`
+      ]
+    },
+    {
+      heading: 'How Do We Protect Your Information?',
+      text: [
+        `The VALUES FOOTPRINT website study team adhere to the principles outlined in Commonwealth legislation regarding the protection and security of personal information.
+        According to these guidelines, the personally identifiable information that is recorded on the VALUES FOOTPRINT site is used only for the purpose stipulated at the time that the information is collected,
+        and cannot be revealed, disclosed, sold, distributed, rented, licensed, shared or otherwise passed on to any third party,
+        unless: (1) we have your express permission to do so; (2) where required by law to provide information in response to a subpoena or warrant,
+        or (3) in cases where the potential risk of harm, or threat to the safety of a consumer or another, requires such disclosure for the purpose of minimising or negating that risk.`,
+        `In order to protect the privacy of consumers, the VALUES FOOTPRINT website uses and implements professional and industry standards for the security and protection of personal information.
+        Current strategies that ensure the highest standards of data security include:`
+      ],
+      list: [
+        `The site is hosted in Australia in a dedicated VDC housed within an ISO27001 accredited data centre. Our VDC employs strict access controls and best practice network layer security`,
+        `Our application has been designed to validate all access and data inputs to protect both the application and database against malicious attacks`,
+        `When using the VALUES FOOTPRINT, users connect to our site via HTTPS, which is a more secure version of the standard HTTP protocol.
+        Our site and the server it resides on are identified and authenticated by SSL certificates in order to use HTTPS,
+        which means users can be assured they are always communicating with the real VALUES FOOTPRINT website.`,
+        `All personally identifiable data is encrypted before it is sent to the database, and this data is stored on a secure database`,
+        `All methods of data transport are secured via SSL`,
+        `Access to data through our application is governed by tightly defined role based access protocols`
+      ]
+    },
+    {
+      heading: 'Cookies',
+      text: [
+        `The VALUES FOOTPRINT website temporarily stores “cookies” on your computer during your visit.
+        These cookies communicate to secure servers, to authenticate access to the website for the duration of your session, and are set to expire when the browsing session ends.
+        The cookies do not collect any information about you, or how you are using your computer, and only serve to ensure that your session authentication has been established.`
+      ]
+    },
+    {
+      heading: 'Your Rights and Responsibilities',
+      text: [
+        `Under the terms of current legislation, you have the right to access, or alter,
+        any information concerning yourself that is recorded on the VALUES FOOTPRINT site.
+        We are only able to access this information if you have provided personally identifiable information, such as an email address.
+        You may request access to this information by emailing valuesfootprint@gmail.com or by clicking the Contact link at the bottom of the home page.
+        If you feel that your privacy has been breached, the contact details for the University’s Privacy Officer are provided below.`,
+        `In order to further protect your privacy, the VALUES FOOTPRINT team suggest consumers follow these guidelines when using the site, or communicating personal information with us:`
+      ],
+      list: [
+        `Use your browser’s private browsing capability if you are concerned about other people looking at your internet history`,
+        `Remember to log out after each session`
+      ]
+    },
+    {
+      heading: 'Further Information',
+      text: [
+        `If you have any questions, issues, or concerns, or wish to lodge a complaint regarding the collection, use, storage,
+        or disclosure of your personal information on the VALUES FOOTPRINT site, please contact the University’s Privacy Officer. Contact details are as follows:`
+      ],
+      list: [
+        `Name: Mr Kornel Koffsovitz`,
+        `Phone: +61 3 9214 5515`,
+        `Email: kkoffsovitz@swin.edu.au`
+      ]
+    },
+    {
+      text: [
+        `Last updated: 6th of November, 2017.`
+      ]
     }
   ]
 };
