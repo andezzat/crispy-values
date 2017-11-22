d749be51ea93b8479b84ef555851ac17cafe362c
Andrew Salib <andezzat@gmail.com>
2017-11-22 14:49:53 +1100

    Reword error page message

--- src/client/pages/Error.jsx
+++ src/client/pages/Error.jsx
@@ -10,7 +10,7 @@ export default class Error extends React.Component {
       <div className="agency-portfolio-hero">
         <section className="container">
           <h1>We've encountered a problem...</h1>
-          <p>Sorry for the inconvenience. If you'd like to report an issue, please click on the bug in the bottom right corner.</p>
+          <p>Sorry for the inconvenience. If you'd like to report an issue, please find the feedback link at the bottom of the page.</p>
           <Row justifyContent="center">
             <Link to="/home" className={cx('btn', 'btn-primary')}>Take me Home</Link>
           </Row>
