a9b24c1343ad6ed7f382b83a23977b38bc54c428
Andrew Salib <andezzat@gmail.com>
2018-02-10 11:50:17 +1100

    change database_example - trivial

--- config/database_example.js
+++ config/database_example.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     production :{
       userName: 'example_user123',
-      password: 'example_password456',
+      password: 'example_password123',
       server: 'example.server.database.com',
       options: { encrypt: true, database: 'exampleDB' }
     }
