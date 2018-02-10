3ff467836f457a4c98108a52c0857b1c70b6d3c4
Andrew Salib <andezzat@gmail.com>
2018-02-10 12:23:37 +1100

    dummy commit

--- config/database_example.js
+++ config/database_example.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     production :{
       userName: 'example_user123',
-      password: 'example_password123',
+      password: 'example_password345',
       server: 'example.server.database.com',
       options: { encrypt: true, database: 'exampleDB' }
     }
